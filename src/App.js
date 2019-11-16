15ce141a4278caa332489c840cb31f3fac318bd3
Marvin Müller <marvin.mueller@code.berlin>
2019-11-16 22:55:08 +0100

    Set the basic structure of the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import './App.css';
+import refreshIcon from './refresh-24px.svg';
+import { tsExpressionWithTypeArguments } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      guilds: null,
+      loading: true,
+    }
+
+    let retrievedGuilds = this.getGuilds();
+
+    this.setState(
+      
+    );
+  }
+
+  getGuilds() {
+    // backend request
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <GuildList guilds={ this.state.guilds }></GuildList>
+      </div>
+    );
+  }
+}
+
+function GuildList(props) {
+  var guildList = [];
+  props.guilds.forEach(guild => {
+    guildList.push(<GuildListItem guild={ guild }></GuildListItem>)
+  });
+}
+
+function RefreshButton(props) {
+  return(
+    <div className="RefreshButton" onClick={ props.onClick }>
+      <img src={ refreshIcon }></img>
     </div>
   );
 }
