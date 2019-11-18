8012b743e63f07342fa1960ad9f0fc482120e904
Marvin Müller <marvin.mueller@code.berlin>
2019-11-18 19:05:49 +0100

    Add mongoDb and make things prettier

--- api/app.js
+++ api/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient();
+const url = "mongodb://localhost:27017/"
 
-// Routes
+// Define Routes
 const guildRoutes = require('./api/routes/guild');
 const userRoutes = require('./api/routes/user');
 
+// Connect Routes
 app.use('/guilds', guildRoutes);
 app.use('users', userRoutes);
 
