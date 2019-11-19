b0e8a46a37ccfafb3edb370c908987bffd949e77
Marvin Müller <marvin.mueller@code.berlin>
2019-11-19 03:48:38 +0100

    Implement api-communication

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import refreshIcon from './refresh-24px.svg';
-import { tsExpressionWithTypeArguments } from '@babel/types';
 const config = require('./config');
 
 class App extends React.Component {
@@ -18,9 +17,33 @@ class App extends React.Component {
     this.UpdateGuilds();
   }
 
+  /* updates the list of guilds in the state */
   UpdateGuilds() {
+    // define a new empty list of guilds
     let guilds = [];
-    fetch(config.apiUrl + "/guilds").then((res) => { console.log(res); });
+
+    // request all guild-Ids
+    fetch(config.apiUrl + "/guilds")
+    // then convert it from json to objects
+    .then(res => res.json())
+    // use that data and take each guild id
+    .then((data) => {
+      data.result.forEach(guildId => {
+        // request each guild from the api
+        fetch(config.apiUrl + "/guilds/" + guildId)
+        // convert it to an object
+        .then(res => res.json())
+        .then((data) => {
+          // extract if from the single-item array and push it to the guilds list
+          guilds.push(data.result[0]);
+        });
+      });
+    });
+
+    // replace the state with the updated list of guilds
+    this.setState({
+      guilds: guilds
+    });
   }
 
   render() {
@@ -44,12 +67,12 @@ function GuildListItem(props) {
   return(<div className="GuildListItem"></div>);
 }
 
-function RefreshButton(props) {
+/* function RefreshButton(props) {
   return(
     <div className="RefreshButton" onClick={ props.onClick }>
-      <img src={ refreshIcon }></img>
+      <img src={ refreshIcon } alt="refreshIcon"></img>
     </div>
   );
-}
+} */
 
 export default App;
