2197a69aad09a11b3cfdc9f5e3ff66b9da345b3a
Marvin Müller <marvin.mueller@code.berlin>
2019-12-03 05:36:49 +0100

    Implement messageCount requests into API

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ app.setDatabase = (database) => {
     userRoutes.setDatabase(database);
     routeRoutes.setDatabase(database);
     channelRoutes.setDatabase(database);
+    messageCountRoutes.setDatabase(database);
 }
 
 // Define Routes
@@ -14,11 +15,13 @@ const guildRoutes = require('./routes/guilds');
 const userRoutes = require('./routes/users');
 const routeRoutes = require('./routes/routes');
 const channelRoutes = require('./routes/channels');
+const messageCountRoutes = require('./routes/messageCount');
 
 // Connect Routes
 app.use('/guilds', guildRoutes);
 app.use('/users', userRoutes);
 app.use('/routes', routeRoutes);
 app.use('/channels', channelRoutes);
+app.use('/messageCount', messageCountRoutes);
 
 module.exports = app;
